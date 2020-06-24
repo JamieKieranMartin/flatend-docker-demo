117a3d1990ec349ee523624b21601ecf6892e175
Jamie Martin <49264143+JamieKieranMartin@users.noreply.github.com>
2020-06-24 18:42:41 +1000

    fixes

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,10 @@
-FROM amd64/alpine:edge
-COPY . . 
-ENTRYPOINT ./flatend
+FROM golang
+# We copy everything into an app directory
+WORKDIR /app
+COPY . .
+# we run go build to compile the binary
+# executable of our Go program
+RUN go build -o main .
+# Our start command which kicks off
+# our newly created binary executable
+CMD "/app/main"
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+
+	"github.com/lithdew/flatend"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	node := &flatend.Node{
+		SecretKey: flatend.GenerateSecretKey(),
+	}
+	defer node.Shutdown()
+
+	node.PublicAddr = "127.0.0.1:9000"
+	check(node.Start())
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt)
+	<-ch
+}
